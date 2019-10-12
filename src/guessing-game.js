d3556ff9deaf5cd43364ac29b0dfd8e9ebfb9b70
Aliaksei Kernoha <halflife.kernoga@yandex.by>
2019-10-13 02:54:25 +0300

    The task is done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.fullMin = min;
+        this.fullMax = max;
     }
 
     guess() {
-
+        return Math.ceil((this.fullMin + this.fullMax) / 2);
     }
 
     lower() {
-
+        this.setRange(this.fullMin , Math.ceil((this.fullMin + this.fullMax) / 2));
     }
 
     greater() {
-
+        this.setRange(Math.ceil((this.fullMin + this.fullMax) / 2) , this.fullMax);
     }
 }
 
